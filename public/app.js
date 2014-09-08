6552154fe373bf2fb3ec1e291e35436dded612a3
Victor Gutierrez <victorgutierrez@outlook.com>
2014-09-08 21:17:43 +0200

    Changes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,29 @@
-angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
-  .config(function() {
-  	
-  });
+angular.module('MyApp', ['ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'MainCtrl'
+      })
+      .when('/shows/:id', {
+        templateUrl: 'views/detail.html',
+        controller: 'DetailCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/signup', {
+        templateUrl: 'views/signup.html',
+        controller: 'SignupCtrl'
+      })
+      .when('/add', {
+        templateUrl: 'views/add.html',
+        controller: 'AddCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+});
